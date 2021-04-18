5c67bb73a7c77fbfd0b31ea4b023d04300475808
Julian Almendarez <ppalmendarez@yahoo.com>
2021-04-18 13:06:28 -0500

    code errors fixed

--- seed.sql
+++ seed.sql
@@ -1,31 +1,39 @@
 CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR (30) NULL,
-PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (30) NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT,
-title VARCHAR (30) NULL,
-salary DECIMAL(10,2) NULL,
-department_id INT NULL,
-PRIMARY KEY (id)
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (30) NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT,
-first_name VARCHAR (30) NULL,
-last_name VARCHAR (30) NULL,
-role_id INT NULL,
-manager_id INT NULL,
-PRIMARY KEY (id)
+CREATE TABLE employees(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    ON UPDATE SET NULL
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    ON UPDATE SET NULL
+    ON DELETE SET NULL
 );
 
 INSERT INTO department (name)
 VALUES ("sales");
 
-INSERT INTO role (title, salary)
+INSERT INTO roles (title, salary)
 VALUES ("sales lead", 1000);
 
-INSERT INTO employee (first_name, last_name)
+INSERT INTO employees (first_name, last_name)
 VALUES ("julian", "almendarez");
